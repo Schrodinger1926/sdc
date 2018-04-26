333e0c0a27d21ffdae25afb6cca620640370cc84
Sidharth Sindhra <sidharth.sindhra@gmail.com>
2018-04-27 03:18:32 +0530

    hough transofrm on lanes

--- ops/detector.py
+++ ops/detector.py
@@ -5,26 +5,13 @@ import cv2
 
 from utils import validator
 
-
 def get_canny_edge_mask(image, kernel_size, low_threshold, high_threshold):
-
-    #convert to grayscale
+    # Convert to grayscale
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     # Remove noise, perfrom gaussian smoothing
     blur_gray = cv2.GaussianBlur(gray, (kernel_size, kernel_size), 0)
-    # Type cast
-    blur_gray_copy = blur_gray.astype(np.uint8)
-    print(blur_gray_copy[])
-    #find connect pixels with grad in b/w both ranges
-    edges = cv2.Canny(blur_gray_copy, low_threshold, high_threshold)
+    # Connected pixels with grad in b/w both ranges
+    edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
 
     return edges
 
-
-"""
-if __name__ == "__main__":
-    edges = get_canny_edge_mask(filename = "exit-ramp.png",
-                                kernel_size = 3,
-                                low_threshold = 100,
-                                high_threshold = 200)
-"""
--- test.py
+++ test.py
@@ -8,42 +8,53 @@ import cv2
 from ops import detector
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-i', '--input')
-parser.add_argument('-o', '--output')
+required_args = parser.add_argument_group("Required arguments")
+required_args.add_argument('-i', '--input', required = True )
+required_args.add_argument('-o', '--output', required = True)
 args = parser.parse_args()
 
 # HACKY ARCHITECHTURE
 filename = args.input
+
 image = cv2.imread(filename)
 
-print(image.shape)
-
 # Get canny edges
-masked_edges = detector.get_canny_edge_mask(image = image,
+edges = detector.get_canny_edge_mask(image = image,
                                             kernel_size = 5,
                                             low_threshold = 50,
                                             high_threshold = 150)
 
+# Next we'll create a masked edges image using cv2.fillPoly()
+mask = np.zeros_like(edges)
+ignore_mask_color = 255
 
-cv2.imwrite('output.jpg', masked_edges)
-quit()
+# This time we are defining a four sided polygon to mask
+imshape = image.shape
+vertices = np.array([[(imshape[1]*0.08, imshape[0]*1),
+                      (imshape[1]*0.48, imshape[0]*0.5),
+                      (imshape[1]*0.52, imshape[0]*0.5),
+                      (imshape[1]*0.92, imshape[0]*1)]], dtype=np.int32)
+
+cv2.fillPoly(mask, vertices, ignore_mask_color)
+masked_edges = cv2.bitwise_and(edges, mask)
+
 # Feed canny edge pixel to hough transform
-rho = 1
-theta = np.pi/180
-threshold = 1
-min_line_length = 10
-max_line_gap = 1
+rho = 2
+theta = 1*(np.pi/180)
+threshold = 200
+min_line_length = 200
+max_line_gap = 30
 line_image = np.copy(image)*0
 
 lines = cv2.HoughLinesP(masked_edges, rho, theta, threshold, np.array([]),
                      min_line_length ,max_line_gap)
 
+assert(lines is not None),"No lines detected at all"
 # Draw lines onto image copy
 for line in lines:
     for x1, y1, x2, y2 in line:
-        cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
+        cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 5)
 
-color_edges = np.dstack((masked_edges, masked_edges, masked_edges))
-
-combo = cv2.addWeighted(color_edges, 0, line_image, 1, 0)
+color_edges = np.dstack((edges, edges, edges))
+combo = cv2.addWeighted(color_edges, 1, line_image, 1, 0)
 cv2.imwrite(args.output, combo)
